dd8e70d9daa7371d5577827b7c1fb5679c6b4c2e
sdlyyxy <sdlyyxy@icloud.com>
2022-03-13 14:10:53 +0800

    Add safari support

--- borderify.js
+++
@@ -1,6 +0,0 @@
-// document.body.style.border = "5px solid red";
-setInterval('document.querySelector("div#con-ar").remove()',1000);
-// document.querySelector("#noscript-warning").remove();
-// document.querySelector(".z-nav").remove();
-
-// js-consent-banner
---
+++ del-baidu-hot/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  del-baidu-hot
+//
+//  Created by sdlyyxy on 2022/3/13.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
